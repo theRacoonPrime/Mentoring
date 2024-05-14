cdb7298e346492ffaca2b44041cb68aef5b46903
zpichles <168102098+zpichles@users.noreply.github.com>
2024-05-15 01:05:48 +0200

    Update task19.py

--- Max/task19.py
+++ Max/task19.py
@@ -5,3 +5,10 @@ sqr=(a+b)*(a+b)
 sum=(a*a)+(b*b)
 print("suma quadratov =",sqr)
 print("quadrat sumy =",sum)
+
+#task vol.2
+print("a,b")
+a, b = int(input()), int(input())
+sqr = (a+b)*(a+b)
+sum = (a*a)+(b*b)
+print(" suma quadratov =",sqr, '\n',"quadrat sumy =",sum)
