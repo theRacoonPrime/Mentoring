3cd39d5b4481c960c51b6d7cd46cae5b10ffa413
Lex0107 <rotakowskiandrey@gmail.com>
2024-05-16 22:28:42 +0200

    Task 24
    Added task 24

---
+++ Aleks/tasks/Task24.py
@@ -0,0 +1,8 @@
+# Variable for number
+number = int(input("Enter the number: "))
+
+# If the sum of the first and the last numbers is equal to the difference between the second and the third numbers, then the answer is YES; if not, then the answer is NO.
+if(int(str(number)[0]) + int(str(number)[3]) == int(str(number)[1]) - int(str(number)[2])):
+    print("YES")
+else:
+    print("NO")
