e0d60020a3ca7febba431341252ff470573e537f
Blablabla585 <106688030+Blablabla585@users.noreply.github.com>
2024-05-19 22:48:56 +0200

    Update task5

--- Ruslan/tasks/task5/JS/script.js
+++ Ruslan/tasks/task5/JS/script.js
@@ -1,10 +1,4 @@
-var x = "";
-var a = 10; //make variable x with value 10
-var x = a; // assigning a to a variable x
-console.log(x); //display value in console
-
-//  there some issue , you not deeply understand what you should to do , you can do it like this for instance 
-// function testA(a) {
-//   var x = a;
-//   return x;
-// }
+function testA(a) {
+  var x = a;
+  return x;
+}
