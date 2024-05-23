44ddf633a504625bdfa5130f8edeb80b07631de9
Andrey Stoletov <stoletov314@gmail.com>
2024-05-23 05:22:28 +0200

    Task 18 and 19 were checked

--- Max/task19.py
+++ Max/task19.py
@@ -6,9 +6,12 @@ sum=(a*a)+(b*b)
 print("suma quadratov =",sqr)
 print("quadrat sumy =",sum)
 
-#task vol.2
-print("a,b")
-a, b = int(input()), int(input())
-sqr = (a+b)*(a+b)
-sum = (a*a)+(b*b)
-print(" suma quadratov =",sqr, '\n',"quadrat sumy =",sum)
+# #task vol.2
+# print("a,b")
+# a, b = int(input()), int(input())
+# sqr = (a+b)*(a+b)
+# sum = (a*a)+(b*b)
+# print(" suma quadratov =",sqr, '\n',"quadrat sumy =",sum)
+
+#  First issue are spaces again , next is that not working , it is just show a nd b , read again task
+#  Solution of this task is really simple
