1b7604fd1358c5517eb3dbee82a8e1f8d0adfd41
Игорь <degtyannikov89@bk.ru>
2024-06-16 19:23:27 +0200

    Done 19 tasks

---
+++ Igor/Task by Andre.py
@@ -0,0 +1,105 @@
+# task_2 = 'Здравствуй мир'
+# print(task_2)
+
+# task_3 = '''
+# 4
+# 8
+# 15
+# 16
+# 23
+# 424
+# 8
+# 15
+# 16
+# 23
+# 42
+# '''
+# print(task_3)
+
+# task_4 = '\n4 \n8 \n15 \n16 \n23 \424 \n8 \n15 \n16 \n23 \n42'
+# print(task_4)
+
+# task_5 = ('\n* \n** \n*** \n**** \n***** \n****** \n*******')
+# print(task_5)
+
+# task_6 = input('Как тебя зовут? ')
+# print('Привет! ' + task_6)
+
+# task_7 = input('Твоя футбольная команда? ')
+# print(task_7 + " - чемпион!")
+
+# task_8_1 = input()
+# task_8_2 = input()
+# task_8_3 = input()
+# answer = (f'\n{task_8_1}\n{task_8_2}\n{task_8_3}')
+# print(answer)
+
+# task_9_1 = input()
+# task_9_2 =input()
+# task_9_3 = input()
+# answer= (f'\n{task_9_3}\n{task_9_2}\n{task_9_1}')
+# print(answer)
+
+# task_10 = ('i', 'like', 'python')
+# print(*task_10, sep='***') /Не совсем понял, как работает, зачем нужна "*" перед"task_10". Обязательно ли sep писать в "print"
+
+# task_11 = input()
+# print("Привет, " + task_11, end='!')
+
+# строка_разделитель = input()
+# task_12_2 = input()
+# task_12_3 = input()
+# task_12_4 = input()
+# task_12 = (task_12_2, task_12_3, task_12_4,)
+# print(*task_12, sep=строка_разделитель)
+
+# task_13 = int(input())
+# second_number = task_13 + 1
+# third_number = second_number + 1
+# print(f'\n{task_13}\n{second_number}\n{third_number}')
+
+
+# task_14 = int(input())
+# task_14_1 = int(input())
+# task_14_2 = int(input())
+# print(task_14 + task_14_1 + task_14_2)
+
+# task_15
+# a = int(input())
+# v = a**3
+# s = 6 * a**2
+# print(v, s)
+
+# #task_16
+# a = int(input())
+# b = int(input())
+# f = 3 * (a + b)**3 + 275 * b**2 - 127 * a - 41
+# print(f)
+
+# task_17 = int(input("Enter your number:"))
+# print(f'Следующее за числом {task_17} число: {task_17 + 1}\nДля числа {task_17} предыдущее число: {task_17 - 1}') #я сам не понял, почему без "f" он пишет что это str (разобрать)
+# print(f'')
+
+# task_18
+# стоимость_монитора = int(input())
+# стоимость_системного_блока = int(input())
+# стоимость_клавиатуры = int(input())
+# стоимость_мыши = int(input())
+# общая_стоимость = стоимость_монитора + стоимость_системного_блока + стоимость_клавиатуры + стоимость_мыши
+# print(общая_стоимость)
+
+# task 18
+# first = int(input())
+# second = int(input())
+# сумма = first + second
+# разность = first - second
+# произведение = first * second
+# print(сумма)
+# print(разность)
+# print(произведение)
+
+
+
+
+
+
