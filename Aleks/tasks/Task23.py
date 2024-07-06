119fc800af322b3bac1f096c6eac21c9eb00c392
theRacoonPrime <stoletov314@gmail.com>
2024-07-06 14:45:24 +0200

    Users folders were deleted

--- Aleks/tasks/Task23.py
+++
@@ -1,8 +0,0 @@
-# Variable for number
-number = int(input("Enter the number: "))
-
-# If the remainder is 1 after taking the modulus with 2, the number is odd; if there is no remainder, the number is even.
-if(number % 2 == 1):
-    print(f"{number} is odd number")
-else:
-    print(f"{number} is even number")
