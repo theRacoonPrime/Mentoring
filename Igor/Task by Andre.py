62b61070baad56a240bd34dada31c07496f3db97
theRacoonPrime <stoletov314@gmail.com>
2024-07-06 14:34:43 +0200

    A few tasks were checked

--- Igor/Task by Andre.py
+++ Igor/Task by Andre.py
@@ -91,35 +91,37 @@
 
 
 # task_18
-monitor_value = int(input())
-system_block_value = int(input())
-keyboard_value = int(input())
-mouse_value = int(input())
-total_value = (monitor_value + system_block_value + keyboard_value + mouse_value) * 3
-print(total_value)
+# monitor_value = int(input())
+# system_block_value = int(input())
+# keyboard_value = int(input())
+# mouse_value = int(input())
+# total_value = (monitor_value + system_block_value + keyboard_value + mouse_value) * 3
+# print(total_value)
 # It is ok , but you can doi it more shortly for instance  print((int(input()) + int(input()) + int(input()) + int(
 # input())) * 3)
 
 
 # task 19
-first = int(input())
-second = int(input())
-amount = first + second
-difference = first - second
-multiplication = first * second
-print(f'{amount}\n{difference}\n{multiplication}')
-#  A little bit long solution , you can do it better , a,b = int(input()),int(input())
-# print(f'{a} + {b} = {a+b}', f'{a} - {b} = {a-b}', f'{a} * {b} = {a*b}', sep='\n')
+# first = int(input())
+# second = int(input())
+# amount = first + second
+# difference = first - second
+# multiplication = first * second
+# print(f'{amount}\n{difference}\n{multiplication}')
+# #  A little bit long solution , you can do it better , a,b = int(input()),int(input())
+# # print(f'{a} + {b} = {a+b}', f'{a} - {b} = {a-b}', f'{a} * {b} = {a*b}', sep='\n')
 
 
 # task_23
 # centimeter = int(input())
 # metr = centimeter * 100
 # print(metr)
+#  Ok
 
 # task_21
-# number = int(input())
-# print(number, number * 2, number * 3, number * 4, number * 5, sep='///')
+number = int(input())
+print(number, number * 2, number * 3, number * 4, number * 5, sep='///')
+#  Ok
 
 # task_24 Я БЛЯТЬ ГОЛОВУ СЕБЕ СЛОМАЛ ПОКА ЭТУ ФОРМУЛУ ПРИДУМАЛ...
 # mandarins = int(input())
@@ -128,19 +130,32 @@ print(f'{amount}\n{difference}\n{multiplication}')
 # result_2 = (int(result_1) * students) - mandarins #Эту...
 # print(int(result_1))
 # print(abs(result_2)) #Я понимаю, что тут другое решение, но я не понимаю, как
-
-#task_25
+#  It is more simple ,  I provide you a few solution how to figure out
 # n = int(input())
-# if n % 2 == 0:
-#     print(n // 2)
-# else:
-#     print(n // 2 + 1)
+# k = int(input())
+# a = k // n
+# b = k % n
+# print(a, b, sep="\n")
 
-#task_20
-# a_1 = int(input('Enter the first element of the arithmetic progression: '))
-# d = int(input('Enter the difference (step) of the arithmetic progression: '))
-# n = int(input('Enter the number of elements of the arithmetic progression: '))
-# a_n = a_1 + d * (n - 1)
-# for item in range (a_1, a_n + 1, d): #Я написал сначала самастоятельно, но потом решил проверить. несмотря на то, что моя прогрессия работала, нейросеть сказала, что я допустил ошибку. я не написал (a_n + 1), написал просто (a_n). Не понимаю,как работает этот +1
-#    print(item)
+#  a, b = int(input()), int(input())
+# print(b // a, b % a, sep = '\n')
 
+ #task_25
+n = int(input())
+if n % 2 == 0:
+    print(n // 2)
+else:
+    print(n // 2 + 1)
+
+#      It is ok , but you can do it without conditionals
+#      n = int(input()) a = n + 1 print(a // 2 )
+
+# task_20 a_1 = int(input('Enter the first element of the arithmetic progression: ')) d = int(input('Enter the
+# difference (step) of the arithmetic progression: ')) n = int(input('Enter the number of elements of the arithmetic
+# progression: ')) a_n = a_1 + d * (n - 1) for item in range (a_1, a_n + 1, d): #Я написал сначала самастоятельно,
+# но потом решил проверить. несмотря на то, что моя прогрессия работала, нейросеть сказала, что я допустил ошибку. я
+# не написал (a_n + 1), написал просто (a_n). Не понимаю,как работает этот +1 print(item) Stop use ChatGPT ,
+# the same example as above , you can do it more simple , something like a1, d, n = int(input()), int(input()),
+# int(input())
+#
+# print(a1 + d * (n - 1))
