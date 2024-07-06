c33bf9dfd58d1f60aab35c8a90f4c27535ce85cc
Игорь <degtyannikov89@bk.ru>
2024-07-06 15:17:03 +0200

    done: 26,27,28,29,30,31,32,33,34,35,36,37,38

--- Igor/Task by Andre.py
+++ Igor/Task by Andre.py
@@ -40,7 +40,6 @@
 # print("Hello, " + task_11, end='!')
 
 
-
 # task_12_1, task_12_2, task_12_3, task_12_4 = input(), input(), input(), input()
 # print(task_12_2, task_12_3, task_12_4, sep=task_12_1)
 
@@ -117,10 +116,107 @@
 #    print(item)
 
 # task_22
-b_1 = int(input('Enter the first term of the progression: '))
-q = int(input('Enter the denominator of progression: '))
-n = int(input('Enter the number of terms of the progression'))
-b_n = b_1 * q ** (n-1) #должно быть в скобках, так как возведение в степень должно быть выполнено до умножения.
-for item in range (b_1, b_n +1, q):
-    print(item)
+# b_1 = int(input('Enter the first term of the progression: '))
+# q = int(input('Enter the denominator of progression: '))
+# n = int(input('Enter the number of terms of the progression'))
+# b_n = b_1 * q ** (n-1) #должно быть в скобках, так как возведение в степень должно быть выполнено до умножения.
+# for item in range (b_1, b_n +1, q):
+#     print(item)
 
+#task_26
+# minute = int(input('write the number of minutes: '))
+# hour = minute // 60
+# hour_left = minute % 60
+# print(f'initial number of minutes {minute} - this {hour} hour {hour_left} minute')
+
+#task_27
+# import math
+# number = int(input())
+# print(math.ceil(number / 4))
+
+#task_28
+# number = int(input())
+# hundreads = number // 100
+# tens = (number // 10) % 10
+# units = number % 10
+# sum_digits = hundreads + tens + units
+# product_digits = hundreads * tens * units
+# print(f'\nsum of digits: {sum_digits}\nnumber production: {product_digits}')
+
+#task_29
+# number = input()
+#
+# for i in range(3):
+#     for j in range(3):
+#         for k in range(3):
+#             if i != j and j != k and k != i:
+#                 print(f"{number[i]}{number[j]}{number[k]}")
+
+# task_32
+# a = int(input())
+# b = int(input())
+# square_of_sum = (a + b) ** 2
+# sum_of_squares = a ** 2 + b ** 2
+# print(f'The squared sum of {a} and {b} is - {square_of_sum} \nSum of squares of {a} and {b} is - {sum_of_squares}')
+
+# task_33
+# a, b, c, d, = int(input()), int(input()), int(input()), int(input())
+# print(a ** b + c ** d)
+
+# task_31
+# height, width = int(input("ENTER len and width")), int(input())
+# for i in range(width): #range создает перебор от 0 до (то число, которые будет вписано) (от 1 до 10 = 0/1/2/3/4/5/6/7/8/9/=)
+#     print("*", end="")
+# print()
+# for i in range(height - 2):
+#     print("*", end="")
+#     for j in range(width-2):
+#         print("/", end="")
+#     print("*")
+# for i in range(width):
+#     print("*", end="")
+
+# task_30
+# number = input()
+# for i in range(4):
+#     for j in range(4):
+#         for y in range(4):
+#             for x in range(4):
+#                 if i != j and j != y and y != x and x != i:
+#                     print(f"{number[i]}{number[j]}{number[y]}{number[x]}")
+
+# task_34
+# number = input()
+# print(int(number) + int(number * 2) + int(number *3))
+
+#task_35
+# qwerty_1 = input()
+# qwerty_2 = input()
+# if qwerty_1 == qwerty_2:
+#     print('password accepted')
+# else: print('password not accepted')
+
+#task 36
+# number = int(input())
+# if number % 2 == 0:
+#     print('even')
+# else: print('odd')
+
+#task_37
+# number = input()
+# first = int(number[0])
+# second = int(number[1])
+# thirty = int(number[2])
+# fourth = int(number[3])
+# sum = first + fourth
+# diif = second - thirty
+# if sum == diif:
+#     print('Yes')
+# else:print('No')
+
+
+#task_38
+# age = int(input())
+# if age >= 18:
+#     print('Access allowed')
+# else: print('Access denied')
