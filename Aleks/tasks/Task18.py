94a71aaba25a8a18fd483dbe4644dc2008d7aff2
theRacoonPrime <stoletov314@gmail.com>
2024-07-06 14:36:48 +0200

    Users folders were deleted

--- Aleks/tasks/Task18.py
+++
@@ -1,4 +0,0 @@
-count = 1
-while count <= 4:
-    print("*" * 17)
-    count += 1
--- Ruslan/tasks/task2/JS/script.js
+++
@@ -1,8 +0,0 @@
-const inputNumber = parseInt(prompt("Введите число"), 10);
-function isEven (number) { //function testing number is even or odd
-    return number % 2 === 0; //checking the modulus after dividing a number by 2
-    }
-console.log(isEven (inputNumber)); //output to console
-
-// Try to figure out why do you need use float  not int and why ?  Can you use int ? 
-//I can use int for integers
