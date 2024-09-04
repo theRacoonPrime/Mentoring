c5dd8e43632aeb6938895c57de2d045ffa0d3caf
Игорь <degtyannikov89@bk.ru>
2024-09-04 19:20:39 +0200

    Task 117

--- Igor/Task by Andre 2.py
+++ Igor/Task by Andre 2.py
@@ -1,1 +1,15 @@
-check
+import math
+
+# class Circle:
+#    def __init__(self, radius): #1. self создается всегда, он пишется т.к функции всегда передается 1 аргумент, это и есть self. 2.мы можем заменить self, для этого, в аргументе через запитую прописывается тот аргумент, который должен заменить self (в нашем случае radius)
+#        self.radius = radius # создаем новое свойство, через "=" прописываем значение, в нашем случае radius
+#метод __init__ — это способ задать начальные настройки и значения для нового объекта, чтобы он был готов к использованию сразу после создания.
+#    def area(self):
+#        return math.pi * (self.radius ** 2) #формула πr²
+#
+#    def perimetr(self):
+#        return 2 * math.pi * self.radius #формула 2πr
+#
+#circle = Circle(5) #Создание объекта Circle с радиусом 5
+#
+#print(f"Circle area: {circle.area()}\nCircle perimeter: {circle.perimetr()}")
