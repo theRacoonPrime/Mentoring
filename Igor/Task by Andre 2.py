8549cc2c3c98efc8eabdcc23be49bfe159563380
Игорь <degtyannikov89@bk.ru>
2024-09-04 19:34:27 +0200

    Task 118

--- Igor/Task by Andre 2.py
+++ Igor/Task by Andre 2.py
@@ -1,6 +1,9 @@
-import math
-
+# task_117
+#
+#import math
+#
 # class Circle:
+#
 #    def __init__(self, radius): #1. self создается всегда, он пишется т.к функции всегда передается 1 аргумент, это и есть self. 2.мы можем заменить self, для этого, в аргументе через запитую прописывается тот аргумент, который должен заменить self (в нашем случае radius)
 #        self.radius = radius # создаем новое свойство, через "=" прописываем значение, в нашем случае radius
 #метод __init__ — это способ задать начальные настройки и значения для нового объекта, чтобы он был готов к использованию сразу после создания.
@@ -13,3 +16,18 @@ import math
 #circle = Circle(5) #Создание объекта Circle с радиусом 5
 #
 #print(f"Circle area: {circle.area()}\nCircle perimeter: {circle.perimetr()}")
+
+# task_118
+#
+# class Student:
+#    def __init__(self, name, grade):
+#        self.name = name
+#        self.grade = grade
+#
+#   def mean(self):
+#        """Calculating grade point average"""
+#        return sum(self.grade) / len(self.grade)
+#
+# student = Student('Igor', [5, 5, 5, 3, 2, 2, 4, 5, 3])
+#
+# print(f'Student Name: {student.name}\n Average grade: {student.mean()}')
