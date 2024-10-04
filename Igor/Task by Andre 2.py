dafbe632f6cca776d88b5628a71e63f4e7e8e9cb
Игорь <degtyannikov89@bk.ru>
2024-10-04 17:24:57 +0200

    # Start work with SQL. Task_11

--- Igor/Task by Andre 2.py
+++ Igor/Task by Andre 2.py
@@ -832,4 +832,17 @@
 # connection.commit()
 #
 # connection.close()
-Check
+
+# Task_11
+
+import sqlite3
+
+dp_path = r'C:\Users\Игорь\PycharmProjects\KNB\Igor\db\northwind.db'
+connection = sqlite3.connect(dp_path)
+cursor = connection.cursor()
+
+cursor.execute('UPDATE Products SET UnitPrice = UnitPrice * 1.10 WHERE CategoryID = 1')
+
+connection.commit()
+
+connection.close()
